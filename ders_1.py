c317d015ccf21e63510278370be973719f54da58
Farid612 <hebibliferid20@gmail.com>
2023-11-18 19:05:18 +0400

    save

--- ders_1.py
+++ ders_1.py
@@ -3,3 +3,51 @@ print("Baki bize miami")
 print(123)
 print(123)
 a = 3 + 4
+
+print("Salam dunya")
+print("Baki bize miami")
+print(123)
+print(123)
+a = 3 + 4
+
+
+print("Salam dunya")
+print("Baki bize miami")
+print(123)
+print(123)
+a = 3 + 4
+
+print("Salam dunya")
+print("Baki bize miami")
+print(123)
+print(123)
+a = 3 + 4
+
+
+print("Salam dunya")
+print("Baki bize miami")
+print(123)
+print(123)
+a = 3 + 4
+
+
+print("Salam dunya")
+print("Baki bize miami")
+print(123)
+print(123)
+a = 3 + 4
+
+
+
+print("Salam dunya")
+print("Baki bize miami")
+print(123)
+print(123)
+a = 3 + 4
+
+print("Salam dunya")
+print("Baki bize miami")
+print(123)
+print(123)
+a = 3 + 4
+
